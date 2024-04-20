65270964abcb01bed657bee7e9f58efba19e2bac
Trinket9 <trinket9gd@gmail.com>
2024-04-21 00:14:15 +0200

    dodaj walidację danych

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,10 +1,19 @@
-﻿namespace apbd_06.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_06.Models;
 
 public class Animal
 {
     public int IdAnimal { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Name { get; set; }
+    [MaxLength(200)]
     public string? Description { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Category { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Area { get; set; }
 }
